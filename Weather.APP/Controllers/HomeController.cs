d68c6e6b477eae5b15cbf8905902c53fd3db2d00
Matan Shulman <matan.shulman87@gmail.com>
2021-11-16 18:47:26 +0200

    final code

--- Weather.APP/Controllers/HomeController.cs
+++ Weather.APP/Controllers/HomeController.cs
@@ -21,13 +21,16 @@ namespace Weather.APP.Controllers
 
         public IActionResult Index()
         {
-           // var s = await GetData();
             return View();
         }
 
+        /// <summary>
+        /// use "api.openweathermap.org" API to get "Tel-Aviv" weather information
+        /// </summary>
+        /// <returns>Root model</returns>
+        [HttpGet]
         public async Task<JsonResult> GetData()
         {
-
             try
             {
                 var httpResponse = await _client.GetAsync("http://api.openweathermap.org/data/2.5/forecast?lat=32.0853&lon=34.7818&appid=4ce97563e2ccf5dc64513ebb6aba9faa&units=metric");
@@ -38,98 +41,15 @@ namespace Weather.APP.Controllers
                 }
                 var content = await httpResponse.Content.ReadAsStringAsync();
                 var tasks = JsonConvert.DeserializeObject<Root>(content);
-
                 return Json(tasks);
             }
 
             catch (Exception ex)
             {
-
-                throw;
+                Response.StatusCode = 500;
+                return Json(ex.Message);
             }
         }
-
-        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
-        public class Main
-        {
-            public double temp { get; set; }
-            //public double feels_like { get; set; }
-            //public double temp_min { get; set; }
-            //public double temp_max { get; set; }
-            //public int pressure { get; set; }
-            //public int sea_level { get; set; }
-            //public int grnd_level { get; set; }
-            //public int humidity { get; set; }
-            //public double temp_kf { get; set; }
-        }
-
-        //public class Weather
-        //{
-        //    public int id { get; set; }
-        //    public string main { get; set; }
-        //    public string description { get; set; }
-        //    public string icon { get; set; }
-        //}
-
-        //public class Clouds
-        //{
-        //    public int all { get; set; }
-        //}
-
-        //public class Wind
-        //{
-        //    public double speed { get; set; }
-        //    public int deg { get; set; }
-        //    public double gust { get; set; }
-        //}
-
-        //public class Sys
-        //{
-        //    public string pod { get; set; }
-        //}
-
-        public class info
-        {
-            //public int dt { get; set; }
-            public Main main { get; set; }
-            //public List<Weather> weather { get; set; }
-            //public Clouds clouds { get; set; }
-            //public Wind wind { get; set; }
-            //public int visibility { get; set; }
-            //public int pop { get; set; }
-            //public Sys sys { get; set; }
-            public string dt_txt { get; set; }
-         //   public DateTime dt_txt { get; set; }
-        }
-
-        public class Coord
-        {
-            public double lat { get; set; }
-            public double lon { get; set; }
-        }
-
-        public class City
-        {
-            //public int id { get; set; }
-            public string name { get; set; }
-            //public Coord coord { get; set; }
-            //public string country { get; set; }
-            //public int population { get; set; }
-            //public int timezone { get; set; }
-            //public int sunrise { get; set; }
-            //public int sunset { get; set; }
-        }
-
-        public class Root
-        {
-            //public string cod { get; set; }
-            //public int message { get; set; }
-            //public int cnt { get; set; }
-            public List<info> list { get; set; }
-            public City city { get; set; }
-        }
-
-
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Weather.APP/Models/WeatherModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Weather.APP.Models
+{
+    public class Main
+    {
+        public double temp { get; set; }
+    }
+    public class info
+    {
+        public Main main { get; set; }
+        public string dt_txt { get; set; }
+    }
+    public class Coord
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+    public class City
+    {
+        public string name { get; set; }
+    }
+    public class Root
+    {
+        public List<info> list { get; set; }
+        public City city { get; set; }
+    }
+}
